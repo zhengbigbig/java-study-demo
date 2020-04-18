181dc152afa803b2f6d9458cb290eb831e73d261
zhengbigbig <780357902@qq.com>
2020-04-18 10:42:11 +0800

    使用依赖注入方式Mock真实环境

--- springboot/src/test/java/com/zbb/controller/ArticleRestControllerTest.java
+++ springboot/src/test/java/com/zbb/controller/ArticleRestControllerTest.java
@@ -4,30 +4,38 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import javax.annotation.Resource;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 //@Transactional 可以使单元测试进行事务回滚，以保证数据库表中没有因测试造成的垃圾数据，因此保证单元测试可以反复执行；
 @Slf4j
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 @SpringBootTest
 public class ArticleRestControllerTest {
 
     //mock对象
+    @Resource
     private MockMvc mockMvc;
 
     //mock对象初始化
-    @BeforeEach
-    public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(new ArticleRestController()).build();
-    }
+//    @BeforeEach
+//    public void setUp() {
+//        mockMvc = MockMvcBuilders.standaloneSetup(new ArticleRestController()).build();
+//    }
 
     //测试方法
     @Test
